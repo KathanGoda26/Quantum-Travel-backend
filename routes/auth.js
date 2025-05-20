d6dfef154549d5612b21ced6642a793757790a91
KathanGoda26 <kathangoda46@gmail.com>
2025-05-21 00:27:05 +0530

    Update auth.js
    second commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { register, login } from '../controllers/authcontroller.js';
 import {
   loginAdmin,
 }  from '../controllers/admincontroller.js';
 
 const router = express.Router();
 
+router.post('/register', register);
+router.post('/login', login);
 router.post('/api/v1', loginAdmin);
 
 export default router;
